aa9230497d144a907c585082833deebe6cee2bab
= <=>
2018-08-22 22:55:28 +0800

    init  code

---
+++ Pthread/Pthread/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pthread
+//
+//  Created by Stephanie on 2018/8/22.
+//  Copyright © 2018年 Stephanie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
